65689dd1b01dce5d63563c80acf8e863d5e08072
Kamil Michalak <kamil.michalak@cgm.com>
2016-03-17 09:27:52 +0100

    WIP

---
+++ include/falling_object.h
@@ -0,0 +1,13 @@
+#ifndef FUR_BLAST_FALLING_OBJECT_H
+#define FUR_BLAST_FALLING_OBJECT_H
+
+class FallingObject {
+  public:
+   FallingObject();
+   virtual ~FallingObject();
+   void fall();
+  private:
+
+};
+
+#endif //FUR_BLAST_FALLING_OBJECT_H
