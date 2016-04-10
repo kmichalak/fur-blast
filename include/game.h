c8327ef41a5af2f70f1a502daee16351e0c7e118
Kamil Michalak <kmichalak8@gmail.com>
2016-04-10 11:10:03 +0200

    Implement main game loop in game.h/game.cpp

--- include/game.h
+++ include/game.h
@@ -1,6 +1,8 @@
 #ifndef FUR_BLAST_GAME_H
 #define FUR_BLAST_GAME_H
 
+#include <SDL.h>
+
 class Game {
 
 public:
@@ -8,7 +10,15 @@ public:
     virtual ~Game();
     void mainLoop();
 private:
+    static const int MIN_FRAME_RATE = 20;
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event *event;
+
     void initialize();
+    void finalize();
+
 };
 
 #endif //FUR_BLAST_GAME_H
--- src/game.cpp
+++ src/game.cpp
@@ -1,3 +1,5 @@
+#include <game_exception.h>
+
 #include "game.h"
 
 Game::Game() {
@@ -6,19 +8,44 @@ Game::Game() {
 
 void Game::initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    this->window =
+    this->window = SDL_CreateWindow("Fur Blast",
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    906, 540,
+                                    SDL_WINDOW_SHOWN);
+    if (!(this->window)) {
+        throw SDLException(SDL_GetError());
+    }
+    this->renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED);
+    this->event = new SDL_Event;
+}
+
+void Game::finalize() {
+    SDL_DestroyRenderer(this->renderer);
+    SDL_DestroyWindow(this->window);
 }
 
 Game::~Game() {
-
+    finalize();
 }
 
 void Game::mainLoop() {
-//    int status = SDL_Init(SDL_INIT_VIDEO);
-//    if (status == 0) {
-//        GameWindow *window = new GameWindow(960, 540, "Fur Blast");
-//        StateManager stateManager(window);
-//        stateManager.run();
-//    }
-//    SDL_Quit();
+
+    while (this->event->type != SDL_QUIT) {
+        Uint32 frameTime = SDL_GetTicks();
+
+        SDL_PollEvent(this->event);
+        SDL_RenderClear(this->renderer);
+
+        // handle input
+        // update game state
+        // redraw all
+
+        SDL_RenderPresent(this->renderer);
+
+        if (SDL_GetTicks() - frameTime < MIN_FRAME_RATE) {
+            SDL_Delay(MIN_FRAME_RATE - (SDL_GetTicks() - frameTime));
+        }
+    }
+
 }
